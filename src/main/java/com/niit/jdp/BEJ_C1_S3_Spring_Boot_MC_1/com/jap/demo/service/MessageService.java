a9eec273a67f7604c9ca045dfdf3da854f4f6040
M V K <krishna.mugala9@gmail.com>
2022-11-17 15:12:38 +0530

    added the message in the main methods using service class

---
+++ src/main/java/com/niit/jdp/BEJ_C1_S3_Spring_Boot_MC_1/com/jap/demo/service/MessageService.java
@@ -0,0 +1,18 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 17-11-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.jdp.BEJ_C1_S3_Spring_Boot_MC_1.com.jap.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+    public String message(){
+        return "Good Day To All";
+    }
+}
